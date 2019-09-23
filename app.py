af53e3319611903f6af01baa911c07a0bdc99443
53X <mirage_1997@mail.ru>
2019-09-23 21:47:39 +0530

    solved most fixes

--- app.py
+++ app.py
@@ -16,23 +16,140 @@ def results():
 
     # fetch action from json
     action = req.get('queryResult').get('action')
-    if action == 'mug':
+    if action == 'mug' or action == 'lanyard' or action == 'insidewall':
         parameters = req.get('queryResult').get('parameters')
         deliverytime = parameters['deliverytime']
-        quantity = parameters['quantity']
-        return {'fulfillmentText': 'Your order for {} mugs with desired delivery time as {} has been successfully placed!!!'.format(quantity, deliverytime)}
+        quantity = int(parameters['quantity'])
+        return {'fulfillmentText': 'Your order for {} {} with desired delivery time as {} has been successfully placed!!!'.format(quantity, action, deliverytime)}
     
     if action == 'folder.folder-yes' or action == 'folder.folder-no':
         parameters = req.get('queryResult').get('outputContexts')[0].get('parameters')
         deliverytime = parameters['deliverytime']
         pouchprint = parameters['pouchprint']
         foldersize = parameters['foldersize']
-        quantity = parameters['quantity']
+        quantity = int(parameters['quantity'])
         papertype = parameters['papertype']
         pouch = parameters['pouch']
-        return {'fulfillmentText': 'Your order for {} folders has been placed. It will be delivered by {}!!!!'.format(quantity, deliverytime)}
+        return {'fulfillmentText': 'Your order for {} folders of size {} has been placed. It will be delivered by {}. Your choice for having "a pouch" and "print on the pouch" in your folder is "{}" and "{}" respectively!!!!'.format(quantity, foldersize, deliverytime, pouch,  pouchprint)}
+    
 
+    if action == 'idcard':
+        parameters = req.get('queryResult').get('parameters')
+        printkind = parameters['printkind']
+        deliverytime = parameters['deliverytime']
+        quantity = int(parameters['quantity'])
+        return {'fulfillmentText': 'Your order for {} {} side printed ID cards with desired delivery time as {} has been successfully placed!!!'.format(quantity,  printkind, deliverytime)}
+
+    if action == 'flyer':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        printkind = parameters['printkind']
+        colortype = parameters['colortype']
+        flyerpaperthick = parameters['flyerpaperthick']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        return {'fulfillmentText': 'Your order for {} {} sized {} flyers with desired delivery time as {} has been successfully placed!!! The print kind of your choice is {} and the thickness of your flyer paper is {}'.format(quantity, papersize, colortype,  deliverytime,  printkind,  flyerpaperthick)}
     # return a fulfillment response
+    
+    if action == 'front':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        stickerkind = parameters['stickerkind']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        return {'fulfillmentText': 'Your order for {} {} sized , {} front gumming stickers with desired delivery time as {} has been successfully placed!!!'.format(quantity, papersize, stickerkind, deliverytime)}
+
+    if action == 'tentcard':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        return {'fulfillmentText': 'Your order for {} {} sized , tent cards with desired delivery time as {} has been successfully placed!!!'.format(quantity, papersize, deliverytime)}
+
+    if action == 'notebook':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        paperthick = parameters['paperthick']
+        pages = parameters['pages']
+        bindingtype = parameters['bindingtype']
+        foiling = parameters['foiling']
+        spotuv = parameters['spotuv']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        
+        return {'fulfillmentText': 'Your order for {} {} sized , {} paged {} notebooks with desired delivery time as {} has been successfully placed!!! The paper thickness of your choice is {}. Your opted "{}" for Foiling and "{}" for SpotUV.'.format(quantity, papersize, pages, bindingtype, deliverytime, paperthick,  foiling,  spotuv)}
+
+    if action == 'sticker':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        stickerkind = parameters['stickerkind']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        cuttingkind = parameters['cuttingkind']
+        laminationkind  = parameters['laminationkind']
+        return {'fulfillmentText': 'Your order for {} {} sized , {} stickers with desired delivery time as {} has been successfully placed!!! The cutting kind of your choice is {} and lamination kind of your choice is {}. Do you want to order anything else ?'.format(quantity, papersize, stickerkind, deliverytime, cuttingkind, laminationkind)}
+
+    if action == 'paperbag':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        thicknesstype = parameters['thicknesstype']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        return {'fulfillmentText': 'Your order for {} {} sized , {} thick paperbag with desired delivery time as {} has been successfully placed!!!'.format(quantity, papersize, thicknesstype, deliverytime)}
+    
+    if action == 'poster':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        sunboard = parameters['sunboard']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        pregum = parameters['pregum']
+        laminationkind  = parameters['laminationkind']
+        return {'fulfillmentText': 'Your order for {} {} sized , {} posters with desired delivery time as {} has been successfully placed!!! Your choice for "pre-gumming" is "{}"" and that for "opting SunBoard" is {}. Do you want to order anything else ?'.format(quantity, papersize, laminationkind, deliverytime, pregum, sunboard)}
+    
+    if action == 'sticker':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        stickerkind = parameters['stickerkind']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        cuttingkind = parameters['cuttingkind']
+        laminationkind  = parameters['laminationkind']
+        return {'fulfillmentText': 'Your order for {} {} sized , {} stickers with desired delivery time as {} has been successfully placed!!! The cutting kind of your choice is {} and lamination kind of your choice is {}. Do you want to order anything else ?'.format(quantity, papersize, stickerkind, deliverytime, cuttingkind, laminationkind)}
+
+    if action == 'banner':
+        parameters = req.get('queryResult').get('parameters')
+        sunboard = parameters['sunboard']
+        startdate = parameters['startdate']
+        enddate = parameters['enddate']
+        return {'fulfillmentText': 'Your order for banner has been successfully placed!!! The start date of your choice is {}, end date of your choice is {}. Your choice for "opting SunBoard" is {}. Do you want to order anything else ?'.format(startdate, enddate, sunboard)}
+    
+    if action == 'scribblepad':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        scribblepadthick = parameters['scribblepadthick']
+        pages = parameters['pages']
+        foiling = parameters['foiling']
+        spotuv = parameters['spotuv']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        
+        return {'fulfillmentText': 'Your order for {} {} sized , {} paged scribble pads with desired delivery time as {} has been successfully placed!!! The paper thickness of your choice is {}. Your opted "{}" for Foiling and "{}" for SpotUV.'.format(quantity, papersize, pages, deliverytime, scribblepadthick, foiling, spotuv)}
+
+    if action == 'billbook':
+        parameters = req.get('queryResult').get('parameters')
+        papersize = parameters['papersize']
+        pouch = parameters['paperthick']
+        colortype = parameters['colortype']
+        startdate = parameters['startdate']
+        enddate = parameters['enddate']
+        deliverytime = parameters['deliverytime']
+        quantity = parameters['quantity']
+        
+        return {'fulfillmentText': 'Your order for {} {} sized , {} bill-books with desired delivery time as {} has been successfully placed!!! The start date of your choice is {} and the desired end date is {}. Your opted "{}" for "Pouch" '.format(quantity, papersize, colortype, deliverytime, startdate, enddate, pouch)}
+
+    
+    
     return {'fulfillmentText': 'This is a response from webhook.'}    
 
 
